3831c2a04257510fab32f847e3ca25269ecac9e6
KIPKIPS <2432719656@qq.com>
2021-03-17 21:39:50 +0800

    快排

---
+++ QuickSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QuickSort {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void QuickSort(int[] array)
+        {
+            Quick();
+        }
+        static void Quick() { }
+    }
+}
