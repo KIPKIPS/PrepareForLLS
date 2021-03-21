53a1e41fd285d8d7cfe7dcd7c8037cc49d1c8735
KIPKIPS <2432719656@qq.com>
2021-03-21 11:01:55 +0800

    归并排序完成

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -3,17 +3,38 @@
 namespace MergeSort {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int[] arr = new[] { 1, 4, 7, 8, 3, 6, 9, 2, 6, 0 };
+            MergeSort(arr, 0, arr.Length - 1);
+            PrintArray(arr);
+            Console.Read();
         }
 
-        static void MergeSort(int[] array) {
-            int mid = array.Length / 2;
-            int[] temp = new int[array.Length];
+        static void MergeSort(int[] array, int left, int right) {
+            if (left >= right) {
+                return;
+            }
+            //将数组分成两半
+            //左排序
+            int mid = left + (right - left) / 2;//中间值
+            MergeSort(array, left, mid);
+            //右排序
+            MergeSort(array, mid + 1, right);
 
-            int i = 0;//指向数组的起点
-            int j = mid + 1;//指向数组中间的索引
+            MergeProcess(array, left, mid + 1, right);//合并起来
+        }
+
+        //一次归并步骤,假设左边和右边的数据都已经是有序的,归并的数组,左指针,右指针,右边界
+        static void MergeProcess(int[] array, int leftPtr, int rightPtr, int border) {
+            if (leftPtr >= rightPtr) {
+                return;
+            }
+            int mid = rightPtr - 1;
+            int[] temp = new int[border - leftPtr + 1];
+
+            int i = leftPtr;//指向数组的起点
+            int j = rightPtr;//指向数组中间的索引
             int k = 0;//新数组的起点
-            while (i <= mid && j < array.Length) {
+            while (i <= mid && j <= border) {
                 if (array[i] <= array[j]) {
                     temp[k] = array[i];
                     i++;
@@ -28,8 +49,12 @@ namespace MergeSort {
                 temp[k++] = array[i++];
             }
 
-            while (j < array.Length) {
+            while (j <= border) {
                 temp[k++] = array[j++];
+            }
+            //将归并好的数据赋值回原数组
+            for (int tempIndex = 0; tempIndex < temp.Length; tempIndex++) {
+                array[leftPtr + tempIndex] = temp[tempIndex];
             }
         }
 
