590947eff3af7957e5e0935584dfd291de08c22f
KIPKIPS <2432719656@qq.com>
2021-03-23 04:12:10 +0800

    座位问题和子项目问题

---
+++ 子项目问题/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 子项目问题 {
+    class Program {
+        static void Main(string[] args) {
+            //处理输入
+            string aList = Console.ReadLine();
+            string bList = Console.ReadLine();
+            string[] bTemp = bList.Split(' ');
+            int[] bData = new int[bTemp.Length];
+            for (int i = 0; i < bTemp.Length; i++) {
+                bData[i] = Convert.ToInt32(bTemp[i]);
+            }
+            string numStr = Console.ReadLine();
+            int num = Convert.ToInt32(numStr);
+            string[] infoList = new string[num];
+            for (int i = 0; i < num; i++) {
+                infoList[i] = Console.ReadLine();
+            }
+
+            Dictionary<int, List<int>> BADic = new Dictionary<int, List<int>>();
+            for (int i = 0; i < infoList.Length; i++) {
+                string[] temp = infoList[i].Split(' ');
+                int a = Convert.ToInt32(temp[0]);
+                int b = Convert.ToInt32(temp[1]);
+                if (!BADic.ContainsKey(b)) {
+                    BADic.Add(b, new List<int>());
+                }
+                if (!BADic[b].Contains(a)) {
+                    BADic[b].Add(a);
+                }
+            }
+            HashSet<int> res = new HashSet<int>();
+            foreach (KeyValuePair<int, List<int>> kvp in BADic) {
+                for (int i = 0; i < kvp.Value.Count; i++) {
+                    if (!res.Contains(kvp.Value[i])) {
+                        res.Add(kvp.Value[i]);
+                    }
+
+                    Console.Write(kvp.Value[i] + " ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine(res.Count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 最大座位距离问题/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// 疫情逐步缓和后，电影院终于开业了，但是由于当前仍处于疫情期间，应尽量保持人群不聚集的原则。
+// 所以当小易来电影院选定一排后，尽量需要选择一个远离人群的位置。
+// 已知由0和1组成的数组表示当前排的座位情况,其中1表示已被选座，0表示空座
+//     请问小易所选座位和最近人的距离座位数最大是多少？
+// 有如下假设：至少有一个人已选座，至少有一个空座位，且座位数限制为 2-1000
+
+namespace 最大座位距离问题 {
+    class Program {
+        static void Main(string[] args) {
+            string[] inp = Console.ReadLine().Split(' ');
+            int max = 0;
+            int tempCount = 0;
+            bool isForward = inp.Length > 0 ? inp[0] == "0" : false;
+            bool isRear = inp.Length > 0 ? inp[inp.Length - 1] == "0" : false;
+            int rearCount = 0;
+
+            bool forwardFlag = false;
+            if (isRear) {
+                for (int i = inp.Length - 1; i >= 0; i--) {
+                    if (inp[i] == "0") {
+                        rearCount++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            int forwordCount = 0;
+            List<int> list = new List<int>();
+            for (int i = 0; i < inp.Length; i++) {
+                if (inp[i] == "0") {
+                    if (i == 0) {
+                        forwardFlag = true;
+                    }
+                    tempCount++;
+                    if (tempCount > max) {
+                        max = tempCount;
+                    }
+
+                } else {
+                    tempCount = 0;
+                    if (forwardFlag) {
+                        forwardFlag = false;
+                        forwordCount = i;
+                    }
+                }
+            }
+
+            //Console.WriteLine(forwordCount + " " + rearCount + " " + max);
+            //Console.WriteLine(isForward + " " + isRear);
+            if (isForward || isRear) {
+                Console.WriteLine(new int[] { forwordCount, rearCount, (int)Math.Ceiling(max / 2f) }.Max());
+            } else {
+                Console.WriteLine((int)Math.Ceiling(max / 2f));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
