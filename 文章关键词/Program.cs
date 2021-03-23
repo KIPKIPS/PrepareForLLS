a7e1e861568b66cfb30a08ddf6a55ef49d8f445c
KIPKIPS <2432719656@qq.com>
2021-03-24 02:56:29 +0800

    关键词问题

---
+++ 文章关键词/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 文章关键词 {
+    class Program {
+        static void Main(string[] args) {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Dictionary<string, int> timesDict = new Dictionary<string, int>();
+            for (int i = 0; i < num; i++) {
+                string lowConvertWord = Console.ReadLine().ToLower();
+                if (!timesDict.ContainsKey(lowConvertWord)) {
+                    timesDict.Add(lowConvertWord, 1);
+                } else {
+                    timesDict[lowConvertWord]++;
+                }
+            }
+
+            int res = 0;
+            foreach (KeyValuePair<string, int> kvp in timesDict) {
+                //Console.WriteLine(kvp.Key + " " + kvp.Value);
+                //Console.WriteLine(kvp.Value / num);
+                if (kvp.Value / (float)num >= 0.01f) {
+                    res++;
+                }
+            }
+
+            Console.WriteLine(res);
+            Console.Read();
+        }
+    }
+}
---
+++ 模拟题/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace 模拟题 {
+    class Program {
+        static void Main(string[] args) {
+            string[] inp = Console.ReadLine().Split(' ');
+            int[] num = new int[inp.Length];
+            int min = Convert.ToInt32(inp[0]);
+            for (int i = 0; i < inp.Length; i++) {
+                int intConvert = Convert.ToInt32(inp[i]);
+                num[i] = intConvert;
+            }
+
+            int res = 0;
+            while (CheckCanAllot(num)) {
+                if (num[0] > num[1]) {
+                    num[0]--;
+                } else {
+                    num[1]--;
+                }
+
+                int max = num[1];
+                int maxIndex = 1;
+                for (int i = 1; i <= 3; i++) {
+                    if (num[i] > max) {
+                        maxIndex = i;
+                        max = num[i];
+                    }
+                }
+                num[maxIndex]--;
+
+                if (num[3] > num[4]) {
+                    num[3]--;
+                } else {
+                    num[4]--;
+                }
+
+                res++;
+            }
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+        }
+
+        static bool CheckCanAllot(int[] array) {
+            if (array[0] == 0 && array[1] == 0) {
+                return false;
+            }
+
+            if (array[1] == 0 && array[2] == 0 && array[3] == 0) {
+                return false;
+            }
+            if (array[3] == 0 && array[4] == 0) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
