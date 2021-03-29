a64ce5c8ed7376641c3f6bfd0ff94403ce5c961b
KIPKIPS <2432719656@qq.com>
2021-03-30 00:03:42 +0800

    中位数问题

--- 寻找两个正序数组的中位数/Program.cs
+++ 寻找两个正序数组的中位数/Program.cs
@@ -3,14 +3,15 @@
 namespace 寻找两个正序数组的中位数 {
     class Program {
         static void Main(string[] args) {
-            int[] num1 = { 1, 3,4};
-            int[] num2 = { 2};
+            int[] num1 = { 1, 3, 4 };
+            int[] num2 = { 2 };
             Console.WriteLine(FindMedianSortedArrays(num1, num2));
             Console.ReadLine();
         }
         public static double FindMedianSortedArrays(int[] nums1, int[] nums2) {
             int totalLength = nums1.Length + nums2.Length;
-            int findIndex = (totalLength - 1) / 2;
+            bool isEven = totalLength % 2 == 0;
+            int findIndex = isEven ? totalLength / 2 : (totalLength - 1) / 2;
             int[] numTotal = new int[totalLength];
             int p1 = 0;
             int p2 = 0;
@@ -20,23 +21,23 @@ namespace 寻找两个正序数组的中位数 {
                         numTotal[i] = nums1[p1];
                         p1++;
                     } else {
-                        numTotal[i] = nums1[p2];
+                        numTotal[i] = nums2[p2];
                         p2++;
                     }
                 } else {
-                    if (p1 >= nums1.Length) {
-                        numTotal[i] = nums1[p1];
+                    if (p1 >= nums1.Length && p2 < nums2.Length) {
+                        numTotal[i] = nums2[p2];
                         p2++;
                     }
 
-                    if (p2 >= nums2.Length) {
-                        numTotal[i] = nums1[p2];
+                    if (p2 >= nums2.Length && p1 < nums1.Length) {
+                        numTotal[i] = nums1[p1];
                         p1++;
                     }
                 }
             }
 
-            if (totalLength % 2 == 0) {
+            if (isEven) {
                 return (numTotal[findIndex] + (double)numTotal[findIndex - 1]) / 2;
             }
             return numTotal[findIndex];
---
+++ 最长回文子串/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace 最长回文子串 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
