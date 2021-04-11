3b26677a5b8335d35b7ac4ea2fa9651596f079ca
KIPKIPS <2432719656@qq.com>
2021-04-12 00:46:27 +0800

    重排最大数

---
+++ 重排最大数/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 重排最大数 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(LargestNumber(new[] { 1, 12, 32, 62, 89 }));
+            Console.Read();
+        }
+        public static string LargestNumber(int[] nums) {
+            List<int> numList = new List<int>();
+            for (int i = 0; i < nums.Length; i++) {
+                string str = nums[i].ToString();
+                for (int j = 0; j < str.Length; j++) {
+                    numList.Add(Convert.ToInt32(str[j].ToString()));
+                }
+            }
+            numList.Sort();
+            numList.Reverse();
+            string res = "";
+            for (int i = 0; i < numList.Count; i++) {
+                res += numList[i];
+            }
+
+            return res;
+        }
+    }
+}
